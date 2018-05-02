b9e7f44f6c28de2e4011edab43f3ce8851824c69
Vivek Jain <jain.vivek.anand@gmail.com>
2018-05-02 23:10:45 +0530

    Updated plotscripts

--- PlotScripts/plotshellTP.sh
+++ PlotScripts/plotshellTP.sh
@@ -1,14 +1,14 @@
-#Mbps
-mkdir plotmeTP
-printf "Flow Throughput (Mbps)\n"
-python calculateTP_A.py N-22-0.pcap
-python calculateTP_B.py N-26-0.pcap
-python calculateTP_C.py N-23-0.pcap
-python calculateTP_D.py N-10-0.pcap
-python calculateTP_E.py N-14-0.pcap
-python calculateTP_F.py N-17-0.pcap
-python calculateTP_G.py N-25-0.pcap
-python calculateTP_H.py N-20-0.pcap
+#Usage startTime
+printf "Flow Throughput\n\n" | tee output.file
+
+python tp-new.py --startTime=2 --destIp 10.0.21.2 N-22-0.pcap plotmeTP/TP-A.plotme plotmeTPP/TP-A.plotme A | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.25.2 N-26-0.pcap plotmeTP/TP-B.plotme plotmeTPP/TP-B.plotme B | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.22.2 N-23-0.pcap plotmeTP/TP-C.plotme plotmeTPP/TP-C.plotme C | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.9.2 N-10-0.pcap plotmeTP/TP-D.plotme plotmeTPP/TP-D.plotme D  | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.13.2 N-14-0.pcap plotmeTP/TP-E.plotme plotmeTPP/TP-E.plotme E | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.16.2 N-17-0.pcap plotmeTP/TP-F.plotme plotmeTPP/TP-F.plotme F | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.24.2 N-25-0.pcap plotmeTP/TP-G.plotme plotmeTPP/TP-G.plotme G | tee output.file
+python tp-new.py --startTime=2 --destIp 10.0.19.2 N-20-0.pcap plotmeTP/TP-H.plotme plotmeTPP/TP-H.plotme H | tee output.file
 
 
 cp gnuplotscript plotmeTP
@@ -16,92 +16,47 @@ cd plotmeTP
 gnuplot gnuplotscript
 cd ..
 
-mkdir plotmeRouterTP
-printf "\n\nRouter Throughput (Mbps)\n"
-printf "R1 "
-python calculateTP_R.py N-0-0.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R1.plotme
+cp gnuplotscriptPackets plotmeTPP
+cd plotmeTPP
+gnuplot gnuplotscriptPackets
+cd ..
 
-printf "R2 "
-python calculateTP_R.py N-1-1.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R2.plotme
+printf "\nRouter Throughput\n\n" | tee output.file
 
-printf "R3 "
-python calculateTP_R.py N-2-1.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R3.plotme
-
-printf "R4 "
-python calculateTP_R.py N-3-1.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R4.plotme
-
-printf "R5 "
-python calculateTP_R.py N-4-1.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R5.plotme
-
-printf "R6 "
-python calculateTP_R.py N-5-1.pcap
-mv plotmeRouterTP/TP.plotme plotmeRouterTP/TP-R6.plotme
+python tp-new.py --startTime=2 N-0-0.pcap plotmeRouterTP/TP-R1.plotme plotmeRouterTPP/TP-R1.plotme R1 | tee output.file
+python tp-new.py --startTime=2 N-1-1.pcap plotmeRouterTP/TP-R2.plotme plotmeRouterTPP/TP-R2.plotme R2 | tee output.file
+python tp-new.py --startTime=2 N-2-1.pcap plotmeRouterTP/TP-R3.plotme plotmeRouterTPP/TP-R3.plotme R3 | tee output.file
+python tp-new.py --startTime=2 N-3-1.pcap plotmeRouterTP/TP-R4.plotme plotmeRouterTPP/TP-R4.plotme R4 | tee output.file
+python tp-new.py --startTime=2 N-4-1.pcap plotmeRouterTP/TP-R5.plotme plotmeRouterTPP/TP-R5.plotme R5 | tee output.file
+python tp-new.py --startTime=2 N-5-1.pcap plotmeRouterTP/TP-R6.plotme plotmeRouterTPP/TP-R6.plotme R6 | tee output.file
 
 cp gnuplotscriptRouters plotmeRouterTP
 cd plotmeRouterTP
 gnuplot gnuplotscriptRouters
 cd ..
 
-mkdir plotmeTPP
-printf "\n\nFlow Throughput (Packets/s)\n"
-python calculateTPP_A.py N-22-0.pcap
-python calculateTPP_B.py N-26-0.pcap
-python calculateTPP_C.py N-23-0.pcap
-python calculateTPP_D.py N-10-0.pcap
-python calculateTPP_E.py N-14-0.pcap
-python calculateTPP_F.py N-17-0.pcap
-python calculateTPP_G.py N-25-0.pcap
-python calculateTPP_H.py N-20-0.pcap
-
-
-cp gnuplotscriptPackets plotmeTPP
-cd plotmeTPP
-gnuplot gnuplotscriptPackets
-cd ..
-
-#PACKETS/sec
-mkdir plotmeRouterTPP
-printf "\n\nRouter Throughput (Packets/s)\n"
-printf "R1 "
-python calculateTPP_R.py N-0-0.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R1.plotme
-
-printf "R2 "
-python calculateTPP_R.py N-1-1.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R2.plotme
-
-printf "R3 "
-python calculateTPP_R.py N-2-1.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R3.plotme
-
-printf "R4 "
-python calculateTPP_R.py N-3-1.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R4.plotme
-
-printf "R5 "
-python calculateTPP_R.py N-4-1.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R5.plotme
-
-printf "R6 "
-python calculateTPP_R.py N-5-1.pcap
-mv plotmeRouterTPP/TP.plotme plotmeRouterTPP/TP-R6.plotme
-
 cp gnuplotscriptRoutersPackets plotmeRouterTPP
 cd plotmeRouterTPP
 gnuplot gnuplotscriptRoutersPackets
+cd ..
+
+mkdir -p ../Graphs
+cp plotmeRouterTP/TP.png ../Graphs/TP-Router.png
+cp plotmeRouterTP/TPPercent.png ../Graphs/TP-Router-Percent.png
+cp plotmeRouterTPP/TP.png ../Graphs/TP-Router-Packets.png
+cp plotmeTP/TP.png ../Graphs/TP-Flow.png
+cp plotmeTP/TPPercent.png ../Graphs/TP-Flow-Percent.png
+cp plotmeTPP/TP.png ../Graphs/TP-Flow-Packets.png
 
 #Cwnd plot
-cd ..
 cp gnuplotscriptQ ../
 cp gnuplotscriptCwnd ../cwndTraces/
 
 cd ..
 gnuplot gnuplotscriptQ
+cp queueSize.png Graphs/
+cp queueStats.txt Graphs/
 
 cd cwndTraces
 gnuplot gnuplotscriptCwnd
+cp Cwnd*.png ../Graphs/
