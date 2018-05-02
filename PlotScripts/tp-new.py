330caf1fa72106ba0774de6578beb4f4f239c4b2
Vivek Jain <jain.vivek.anand@gmail.com>
2018-05-02 23:29:33 +0530

    Fix plotscript for router packet TP

--- PlotScripts/tp-new.py
+++ PlotScripts/tp-new.py
@@ -59,7 +59,7 @@ with open(file_name[0:file_name.rindex('.')] + '.csv') as fq:
             if (new_time - old_time > 0.1):
                 val = val/(new_time - old_time)
                 fBytes.write(str(p) + " " + str(val * 8.0/(1000.0 * 1000.0)) + "\n")
-                fPackets.write(str(p) + " " + str(val) + "\n")
+                fPackets.write(str(p) + " " + str(valPacket) + "\n")
                 p += (new_time - old_time)
                 old_time = new_time
                 val = 0
