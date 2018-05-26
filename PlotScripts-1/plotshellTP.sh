41313e64d5f18894f997d8bf7fcd70f2e9255101
Viyom <viyommittal@gmail.com>
2018-05-26 21:42:01 +0530

    Add queue traces and PI configuration parameters for GFC-1

--- PlotScripts-1/plotshellTP.sh
+++ PlotScripts-1/plotshellTP.sh
@@ -80,8 +80,37 @@ cd ..
 cp gnuplotscriptQ ../
 cp gnuplotscriptCwnd ../cwndTraces/
 
+cd ../queueTraces/
+echo "Queue 1" > QueueStatsAfter2sec.txt
+drops=`awk '{if ($1 >= 2) print $0}' drop-0.plotme | wc -l`
+marks=`awk '{if ($1 >= 2) print $0}' mark-0.plotme | wc -l`
+echo -e  "$drops Drops, $marks Marks\n" >> QueueStatsAfter2sec.txt
+
+echo "Queue 2" >> QueueStatsAfter2sec.txt
+drops=`awk '{if ($1 >= 2) print $0}' drop-1.plotme | wc -l`
+marks=`awk '{if ($1 >= 2) print $0}' mark-1.plotme | wc -l`
+echo -e  "$drops Drops, $marks Marks\n" >> QueueStatsAfter2sec.txt
+
+echo "Queue 3" >> QueueStatsAfter2sec.txt
+drops=`awk '{if ($1 >= 2) print $0}' drop-2.plotme | wc -l`
+marks=`awk '{if ($1 >= 2) print $0}' mark-2.plotme | wc -l`
+echo -e  "$drops Drops, $marks Marks\n" >> QueueStatsAfter2sec.txt
+
+echo "Queue 4" >> QueueStatsAfter2sec.txt
+drops=`awk '{if ($1 >= 2) print $0}' drop-3.plotme | wc -l`
+marks=`awk '{if ($1 >= 2) print $0}' mark-3.plotme | wc -l`
+echo -e  "$drops Drops, $marks Marks\n" >> QueueStatsAfter2sec.txt
+
+cp QueueStatsAfter2sec.txt ../Graphs/
+
 cd ..
 gnuplot gnuplotscriptQ
 
 cd cwndTraces
 gnuplot gnuplotscriptCwnd
+
+cd ..
+mkdir Graphs
+cp pcap/*/*.png Graphs
+cp queueSize.png Graphs
+cp cwndTraces/*.png Graphs
